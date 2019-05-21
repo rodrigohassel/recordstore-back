6d297048e1739ab71d6fc426221c4bb72960ea3e
Rodrigo Hassel <rodri.hassel@gmail.com>
2019-05-21 11:27:45 -0300

    First lesson from the course Ruby onails API with vue.js

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :records
+      resources :artists
+    end
+  end
+
+  root to: "home#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
